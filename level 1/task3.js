cdd5eaf8049fa70923e4e7cf880d565f4f4d4876
mdrifatunnobirifat <mdrifatunnabi@gmail.com>
2026-02-01 15:43:57 +0600

    :rocket: Deploy:level 1 task 3

---
+++ level 1/task3.js
@@ -0,0 +1,12 @@
+let temp=prompt("enter the temperature in celcius");
+
+let c=temp;
+
+let f;
+f=(c*9/5)+32;
+let k; 
+k=c+273.15;
+console.log("Celsisus:"+c+" C");
+console.log("Farenheit"+f+" F");
+console.log("kelvin"+k+" K");
+
