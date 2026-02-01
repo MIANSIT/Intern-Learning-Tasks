88f74f4fe3b837e7ec8fcecec8c2be2cd1665ce3
mdrifatunnobirifat <mdrifatunnabi@gmail.com>
2026-02-01 17:05:49 +0600

    :rocket: Deploy:level 2 task 6

---
+++ level 2/task6.js
@@ -0,0 +1,16 @@
+let marks=prompt("enter marks(0-100):");
+
+let m=marks;
+
+if(m>=0 &&m<=50)
+{alert("F");}
+else if(m>=51 &&m<=59)
+{alert("D");}
+else if(m>=60 && m<=69)
+{alert("C");}
+else if(m>=70 && m<=79)
+{alert("B");}
+else if(m>=80 && m<=89)
+{alert("A");}
+else if(m>=90 && m<=100)
+{alert("A+");}
