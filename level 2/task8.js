df4f768c2df7230f7781e98370ccea0a2f466adf
mdrifatunnobirifat <mdrifatunnabi@gmail.com>
2026-02-01 17:53:53 +0600

    :rocket: Deploy:level 2 task 8

---
+++ level 2/task8.js
@@ -0,0 +1,15 @@
+const age=prompt("enter age to but tickit: ");
+
+if(age<5)
+{alert("Free");}
+else if(age>=5 && age<=12)
+{alert("ticket type=child  , price=100 BDT");}
+else if(age>=13 && age<=17)
+{alert("ticket type=Teen  , price=150 BDT");}
+else if(age>=18 && age<=59)
+{alert("ticket type=Adult  , price=200 BDT");}
+else if(age>=60)
+{alert("ticket type=Senior  , price=120 BDT");}
+
+
+
