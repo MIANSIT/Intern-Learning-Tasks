387c899daac353f25dce03b89c13ee954deb8084
mdrifatunnobirifat <mdrifatunnabi@gmail.com>
2026-02-01 16:42:12 +0600

    :rocket: Deploy:level 1 task 5

---
+++ level 1/task5.js
@@ -0,0 +1,14 @@
+let text=prompt("enter the string:");
+
+let t;
+t=text;
+len=[text];
+last=text.length;//to calcutae the lastword
+let upcase=t.toLowerCase();
+let lowcase=t.toUpperCase();
+console.log("inputed text:"+t);
+console.log("uppercase: "+upcase);
+console.log("lowercase: "+lowcase);
+console.log("first letter:"+text[0]);
+console.log("last letter:"+text[last-1]);//array is always (-1) to lenght
+//console.log(""+t[]);
