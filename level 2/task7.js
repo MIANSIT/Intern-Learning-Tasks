8e6e937f6f839814c47acf8ec0813d0a31ce344e
mdrifatunnobirifat <mdrifatunnabi@gmail.com>
2026-02-01 17:34:55 +0600

    :rocket: Deploy:level 2 task 7

---
+++ level 2/task7.js
@@ -0,0 +1,13 @@
+let username=prompt("enter your username");
+let password=prompt(" enter password");
+
+if(username=="admin" && password=="1234")
+{
+    alert("Login Successful");
+}
+else
+{
+    alert("invalid credentials");
+}
+
+
