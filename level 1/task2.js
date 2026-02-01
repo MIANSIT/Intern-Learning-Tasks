6b7974b1ec29fe0d5aa93f6dcad2331ed79be40f
mdrifatunnobirifat <mdrifatunnabi@gmail.com>
2026-02-01 15:26:30 +0600

    :rocket: Deploy:level 1 task 2

---
+++ level 1/task2.js
@@ -0,0 +1,18 @@
+let fn=prompt("enter first number: ");
+let sn=prompt("enter second number: ");
+
+let a=fn;
+let b=sn;
+let sum,sub,mul,div;
+sum=a+b;
+sub=a-b;
+mul=a*b;
+div=a/b;
+let value;
+
+console.log("first number:"+a);
+console.log("second number:"+b);
+console.log("Addition:"+sum);
+console.log("Subtraction:"+sub);
+console.log("Multiplication:"+mul);
+console.log("Dicision:"+div);
