a62e2220630b2e26c7513a36091cf8c06d3a44d8
mdrifatunnobirifat <mdrifatunnabi@gmail.com>
2026-02-01 14:43:16 +0600

    :rocket: Deploy:level 1 task 1

---
+++ level 1/task1.js
@@ -0,0 +1,8 @@
+const name="Rifat";
+let age,city,hobby;
+age=24;
+city="Dhaka";
+hobby="Gaming";
+
+console.log(name,age, city,hobby);
+alert("name"+name+" age:"+age+" city:"+city+" hobby:"+hobby);
