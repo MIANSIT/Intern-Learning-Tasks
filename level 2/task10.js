619ed00a6dae5771bfb514df3e6ec22f5df722ec
mdrifatunnobirifat <mdrifatunnabi@gmail.com>
2026-02-01 19:20:10 +0600

    :sparkles: Feat:level 2 task 10

---
+++ level 2/task10.js
@@ -0,0 +1,28 @@
+let dname=prompt("enter the day name:");
+
+switch(dname)
+{
+    case "Saturday":
+      alert("Weekday");
+      break;
+    case "Sunday":
+      alert("Weekday");
+      break;
+    case "Monday":
+      alert("Weekday");
+      break;
+    case "Tuesday":
+      alert("Weekday");
+      break;
+    case "Wednesday":
+      alert("Weekday");
+      break;
+    case "Thrusday":
+      alert("Weekday");
+      break;
+    case "Friday":
+      alert("Weekend");
+      break;
+    default:
+        alert("try again with the capital for the first letter")
+}
