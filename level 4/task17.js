4348f74c29c6cc8aaac9e9c3814578117e9dfb7c
mdrifatunnobirifat <mdrifatunnabi@gmail.com>
2026-02-02 21:22:10 +0600

    :sparkles: Feat:level 4 task 17

---
+++ level 4/task17.js
@@ -0,0 +1,15 @@
+const value=new Promise((resolve,reject)=>
+    {
+      const number=Math.random();
+      if(number>0.5)
+      {
+        resolve("Success!");
+      }
+      else{
+        reject("Failed!");
+      }
+    }
+);
+
+value .then((result)=> {console.log(result);})
+.catch((error)=>{console.log(error);}); 
