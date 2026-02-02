a7360e2ccfab9db3d8bffeee1a4381b3359db13a
mdrifatunnobirifat <mdrifatunnabi@gmail.com>
2026-02-02 15:36:20 +0600

    :sparkles: Feat:level 3 task 12

---
+++ level 3/task12.js
@@ -0,0 +1,11 @@
+let num= prompt("enter the number to get total from 1 to this number:");
+
+let number=num;
+let sum;
+
+for(let i=1;i<=number;i++)
+{
+     sum=(i*(i+1))/2;// n(n+1)/2
+   
+}
+ console.log("total="+sum);
