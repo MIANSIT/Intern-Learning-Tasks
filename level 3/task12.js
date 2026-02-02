b7bfb834332e3b464c23a5512d4530a28fc49b81
mdrifatunnobirifat <mdrifatunnabi@gmail.com>
2026-02-02 15:44:27 +0600

    :sparkles: Feat:level 3 task 12with bonus task

--- level 3/task12.js
+++ level 3/task12.js
@@ -1,11 +1,12 @@
 let num= prompt("enter the number to get total from 1 to this number:");
 
 let number=num;
-let sum;
+let sum,avg;
 
 for(let i=1;i<=number;i++)
 {
      sum=(i*(i+1))/2;// n(n+1)/2
-   
+     avg=((i*(i+1))/2)/i; // normal average
 }
  console.log("total="+sum);
+  console.log("total="+avg);
